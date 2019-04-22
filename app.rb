eee0ac96de29aead8cbc37c86ec487992d970b5d
Patrick McCombs <pa.mccombs1@gmail.com>
2019-04-22 10:46:39 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,8 +7,8 @@ class App < Sinatra::Base
   	params[:name].reverse
   end
   
-  get "/multiply/:num1/:num2" do
-  	@product = params[:num1].to_i * params[:num2].to_i
-  	"#{@product}"
+  get "/square/:number" do
+  	params[:number] * params[:number]
+  end
 
 end
