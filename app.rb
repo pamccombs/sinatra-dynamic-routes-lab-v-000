0411c54247b87f99a84396e5d4cdc4620324af88
Patrick McCombs <pa.mccombs1@gmail.com>
2019-04-22 10:59:20 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -25,7 +25,7 @@ class App < Sinatra::Base
     "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
   
-  get "/" do
+  get "/:operation/:number1/:number2" do
     
   end
 
